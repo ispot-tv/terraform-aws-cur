a4a1a2bc4816eb46e7a3cf2f4584ed862746c6bf
Almenon <almenon214@gmail.com>
2023-06-10 10:18:44 -0700

    update lambda

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 const AWS = require('aws-sdk');
-
+const response = require('./cfn-response');
 exports.handler = function(event, context, callback) {
+  if (event.RequestType === 'Delete') {
+    response.send(event, context, response.SUCCESS);
+  } else {
     const glue = new AWS.Glue();
-    
-    glue.startCrawler({ Name: process.env.CRAWLER_NAME }, function(err, data) {
-        if (err) {
-            // Check if Crawler is already running
-            const response = JSON.parse(this.httpResponse.body);
-            if (response['__type'] == 'CrawlerRunningException') {
-                console.log('Crawler already running; ignoring trigger.');
-
-                callback(null, response.Message);
-            }
+    glue.startCrawler({ Name: 'AWSCURCrawler-aws-team' }, function(err, data) {
+      if (err) {
+        const responseData = JSON.parse(this.httpResponse.body);
+        if (responseData['__type'] == 'CrawlerRunningException') {
+          callback(null, responseData.Message);
+        } else {
+          const responseString = JSON.stringify(responseData);
+          if (event.ResponseURL) {
+            response.send(event, context, response.FAILED,{ msg: responseString });
+          } else {
+            callback(responseString);
+          }
         }
-        else {
-            console.log("Successfully triggered crawler");
+      }
+      else {
+        if (event.ResponseURL) {
+          response.send(event, context, response.SUCCESS);
+        } else {
+          callback(null, response.SUCCESS);
         }
+      }
     });
-}
+  }
+};
